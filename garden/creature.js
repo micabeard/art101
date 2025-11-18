59c3e0e4d2e316396b04dac551ed2459f9d03787
micabeard <micaelabbeard@gmail.com>
2025-11-18 14:52:37 -0800

    h

--- garden/creature.js
+++ garden/creature.js
@@ -1,14 +1,45 @@
-//a variable for storing names
-
-//when the button is clicked (event) 
-
 $("#add-creature").click(
-function () {
-    alert("button is clicked");
+    function () {
 
-//reach for this input field and grab the text from it;
-let crName = $("#crName").val();
-$("#creature-list").append(crName);
-});
-//the magic move 
-//
+        // grab the value from the input
+        let crName = $("#crName").val();
+        let crColor= $("#crColor").val();
+        let crEyesNum= $("#crEyesNum").val();
+        let crEyesHTML="";
+
+
+        for (let i = 0; i < crEyesNum ; i++){
+            crEyesHTML=crEyesHTML + "<div class=eye>.</div>";
+        }
+
+let mouthHTML = `<div class="mouth">u</div>`;
+
+    if (crName === "" || crName.length > 12) {
+        return;
+    }
+
+    $("#creature-list").append(`
+        <div class="creature">
+            <div class="creature-body" style="background: ${crColor}">
+                <div class="eyes">${crEyesHTML}
+                ${mouthHTML}
+            </div>
+            <div class="creature-info">${crName}</div>
+        </div>
+    `);
+        // check for the field value do not add empty ones
+        if ( (crName == "") || (crName.length>12) ) { // do nothing 
+        }
+        else {
+            $("#creature-list").append(`
+<div class="creature">
+    <div class="creature-body" style="background: ${crColor}">${crEyesHTML}</div>
+    <div class="creature-info">${crName}</div>
+</div>
+`);
+        }
+
+        // remove the name after it's added
+        $("#crName").val("");
+
+    });
